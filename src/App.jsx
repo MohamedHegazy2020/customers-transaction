319099fdc9e9f97abb8bfd48276a805c4274e3ea
MohamedHegazy2020 <mohamedhegazy219427@gmail.com>
2024-07-14 00:13:26 +0300

    finsih assignment

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import CustomerTable from "./components/CustomerTable"
+
+const App = () => {
+  return (
+    <div className="container">
+      <CustomerTable/>
+    </div>
+  )
+}
+
+export default App
